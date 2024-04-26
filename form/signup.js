5f14854b92d0877231aa8bc02b1ccd758b6dff71
dinadehbi <147596316+dinadehbi@users.noreply.github.com>
2024-04-26 13:32:03 +0100

    Update signup.js

--- form/signup.js
+++ form/signup.js
@@ -2,6 +2,8 @@ const myForm = document.getElementById('myForm');
 const myForm2 = document.getElementById('secondForm');
 const Ereur = document.getElementById('Ereur');
 const lienLogIn = document.getElementById("lienLogin");
+const logInButton = document.getElementById("LogIn");
+const signUpButton = document.getElementById("signup");
 
 lienLogIn.addEventListener("click", function(e){
     e.preventDefault(); 
@@ -9,6 +11,7 @@ lienLogIn.addEventListener("click", function(e){
     myForm2.style.display = "block";
     
 })
+signUpButton.addEventListener("click",function(){
 
 myForm.addEventListener('submit', function(event) {
     event.preventDefault();
@@ -30,43 +33,53 @@ myForm.addEventListener('submit', function(event) {
             myForm2.style.display="block";
             console.log(data);
 
-            myForm2.addEventListener('submit', function(e) {
-                e.preventDefault();
-                var formData2 = new FormData(this);
+        }else {
+            Ereur.style.display="block";
+            Ereur.innerHTML = data;
+            console.log(data);
             
-                fetch('signup.php', {
-                    method: 'POST',
-                    body: formData2
-                })
-                .then(response2 => {
-                    if (!response2.ok) {
-                        alert('Network response was not ok');
-                    }
-                    return response2.text();
-                })
-                .then(logInData=> {
-                    if (logInData === 'true') {
-                        console.log("welcome");
-                    } else {
-                        console.log(logInData);
-                    }
-                })
-                .catch(error => {
-                    console.error('Error:', error);
-                });
-            });
+            }
             
-        } else {
-           Ereur.style.display="block";
-           Ereur.innerHTML = data;
-           console.log(data);
-
-        }
+            
     })
     .catch(error => {
         console.error('Error:', error);
     });
 });
+})
 
 
+logInButton.addEventListener("click", function(){
+    myForm2.addEventListener('submit', function(e) {
+        e.preventDefault();
+        var formData2 = new FormData(this);
 
+        fetch('login.php', {
+            method: 'POST',
+            body: formData2
+        })
+        .then(response2 => {
+            if (!response2.ok) {
+                alert('Network response was not ok');
+            }
+            return response2.text();
+        })
+        .then(data=> {
+        
+            if (data === 'true') {
+                myForm2.style.display="none";
+                console.log(data);
+
+            }else {
+                Ereur.style.display="block";
+                Ereur.innerHTML = data;
+                console.log(data);
+                
+                }
+            
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    });
+})
